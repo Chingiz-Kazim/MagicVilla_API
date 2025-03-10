318ccd8cef8b1d442c04157631b5a3572a560fef
Chingiz-Kazim <kazimov.chingiz.111@gmail.com>
2025-03-10 15:27:26 +0400

    Custom Logger

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -1,12 +1,21 @@
+﻿
+using MagicVilla_VillaAPI.Logging;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//если есть необходимость сторонним библиотекам, которые ведут логгирование,
+//помимо необходимо установить через Nuget_Manager SeriLog.AspNetCore & SeriLog.Sinks.File
 
+//Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.File("log/villaLogs.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+
+//builder.Host.UseSerilog();
+
+// Add services to the container.
 builder.Services.AddControllers(option => { /*option.ReturnHttpNotAcceptable = true;*/ }).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddSingleton<ILogging, LoggingV2>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
